5a0bd4c226755aa0a36321a719ea42dc0536c5ee
Jochen Mariën <jochen.marien@thomasmore.be>
2024-01-15 14:13:30 +0100

    init

---
+++ refactor.py
@@ -0,0 +1,5 @@
+from_to = {
+"https://www.landleren.be/": "https://landleren.digitalinnovation.be/",
+"https://status.landleren.be/": "https://status-landleren.digitalinnovation.be/",
+"https://static.landleren.be/": "https://static-landleren.digitalinnovation.be/"
+}
